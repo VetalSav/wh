e2b5a17e8999c260cae322ae59dd3f2124354f8f
Vitali <110376993+VetalSav@users.noreply.github.com>
2022-12-17 12:08:14 +0300

    HW7

--- src/s2-homeworks/hw07/HW7.tsx
+++ src/s2-homeworks/hw07/HW7.tsx
@@ -12,13 +12,13 @@ import s from './HW7.module.css'
 * */
 
 const arr = [
-    { id: 1, value: 'x' },
-    { id: 2, value: 'y' },
-    { id: 3, value: 'z' },
+    { id: 1, value: 'Pre-Junior', name:'skill',},
+    { id: 2, value: 'Junior' , name:'skill',},
+    { id: 3, value: 'Junior+',name:'skill' ,},
 ] // value может быть изменено
 
 const HW7 = () => {
-    const [value, onChangeOption] = useState(1) // селект и радио должны работать синхронно
+    const [value, onChangeOption] = useState('Pre-Junior') // селект и радио должны работать синхронно
 
     return (
         <div id={'hw7'}>
