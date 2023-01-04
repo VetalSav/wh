33400bff5b2e71795237ce512287b68a40e8b474
Vitali <110376993+VetalSav@users.noreply.github.com>
2023-01-04 19:02:49 +0300

    HW9_end

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -8,29 +8,25 @@ function Clock() {
     // for autotests // не менять // можно подсунуть в локалСторэдж нужную дату, чтоб увидеть как она отображается
     const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())))
     const [show, setShow] = useState<boolean>(false)
-
-
-    const time = (timerId: any) => {
-        setInterval(() => setDate(new Date(restoreState('hw9-date', Date.now()))), timerId)
-    };
-
-    timerId && time(timerId)
-
+console.log(timerId)
     const start = () => {
-        setTimerId(1000)
-
         // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
         // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
-
-
+        const id: number = +setInterval(() => {
+            setDate(new Date())
+        }, 1000)
+        setTimerId(id)
     }
 
     const stop = () => {
-        setTimerId(undefined)
-
         // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
-
+        if (timerId) {
+            clearInterval(timerId)
+            setTimerId(undefined)
+        }
     }
+
+
 
     const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
         setShow(true)
@@ -44,7 +40,7 @@ function Clock() {
         second: "numeric"
     });
     const stringTime = formatter.format(date) || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = date.toLocaleString("ru-Ru", {day:'numeric',month:'numeric',year:'numeric'}) || <br/>
+    const stringDate = date.toLocaleString("ru-Ru", {day: 'numeric', month: 'numeric', year: 'numeric'}) || <br/>
         || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
